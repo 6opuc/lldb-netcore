71fd0e3244c6ba0ec6e42f3cf3fac6b02a7be979
Jamie Snell <jamie.snell@hudl.com>
2020-01-29 13:01:18 -0600

    netcore 2.2.8

--- build.sh
+++ build.sh
@@ -2,6 +2,6 @@
 
 docker build \
 	--tag 6opuc/lldb-netcore \
-	--build-arg BASE_IMAGE=mcr.microsoft.com/dotnet/core/sdk:2.2.401 \
-    --build-arg CORECLR_BRANCH=v2.2.6 \
+	--build-arg BASE_IMAGE=mcr.microsoft.com/dotnet/core/sdk:2.2.207 \
+    --build-arg CORECLR_BRANCH=v2.2.8 \
 	.
